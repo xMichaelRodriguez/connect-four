96dda0180a9f8ca81ea6f745c1c17aeb56769456
xMichaelRodriguez <scottlovos503@gmail.com>
2022-05-28 11:36:30 -0600

    probando subida de dos carpetas

--- server/build/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require(".");
-_1.app.listen(_1.app.get("port"), () => {
-    console.log(`Server started on port ${_1.app.get("port")}`);
-});
--- server/build/routes/colaRoute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.listen = void 0;
-const listen = (io, _) => {
-    io.emit("message", "Hello from server");
-};
-exports.listen = listen;
