13499d1bf04b4b7759e61f457e6f4a665ccf7a8f
xMichaelRodriguez <scottlovos503@gmail.com>
2022-06-20 16:38:44 -0600

    entorno de desarrollo

--- client/src/__tests__/App.test.tsx
+++ client/src/__tests__/App.test.tsx
@@ -1,3 +1,11 @@
-it('should true', () => {
-  expect(true).toBe(true);
+import { render } from '@testing-library/react';
+import { AuthPage } from '../features/Auth/pages/AuthPage';
+
+describe('Test on Login Screen', () => {
+  it('The component should be defined', () => {
+    const component = render(<AuthPage />);
+    const { container } = component;
+
+    expect(container).toBeDefined();
+  });
 });
