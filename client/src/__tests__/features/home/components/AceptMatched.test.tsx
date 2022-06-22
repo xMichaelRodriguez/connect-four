1e1f44297583bd73a3638cfdd7f25fe1e01fd6d8
xMichaelRodriguez <scottlovos503@gmail.com>
2022-06-22 15:10:15 -0600

    Trantando de arreglar el error de el onclose

--- client/src/__tests__/features/home/components/AceptMatched.test.tsx
+++ client/src/__tests__/features/home/components/AceptMatched.test.tsx
@@ -9,22 +9,32 @@ import { AcepMatched } from '../../../../features/home/components/AcepMatched';
  */
 describe('Test on <AceptMatched/>', () => {
   const onAcept = vitest.fn();
-  const onRejected = vitest.fn();
-
+  const obj = {
+    onRejected: vitest.fn(),
+    onClose: vitest.fn(),
+  };
+  const { onRejected } = obj;
   const component = render(<AcepMatched onAcepted={onAcept} onRejected={onRejected} />);
   it('The component should be defined', () => {
     expect(component.container).toBeDefined();
   });
 
   it('The accept button should work', async () => {
-    await act(async () => userEvent.click(component.getByText('Accept')));
+    await act(async () => {
+      await userEvent.click(component.getByText('Accept'));
+    });
     expect(onAcept).toHaveBeenCalled();
   });
-  it('The Reject button should work', async () => {
-    await act(() => {
-      userEvent.click(component.getByText('Reject'));
-    });
 
-    expect(onRejected).toHaveBeenCalled();
+  it('The Reject button should work', async () => {
+    await act(async () => {
+      await userEvent.click(component.getByText('Reject'));
+    });
+    const spyOnReject = vitest.spyOn(obj, 'onRejected');
+    const spyOnClose = vitest.spyOn(obj, 'onClose');
+    obj.onRejected();
+    obj.onClose();
+    expect(spyOnReject).toHaveBeenCalled();
+    expect(spyOnClose).toHaveBeenCalled();
   });
 });
